f12921dc4daf6bc1ed7df3c44abb8b33180fe2d8
savita1378 <savita1378@iiitd.ac.in>
2014-12-10 20:28:46 +0530

    initial checkin of sales tool

---
+++ gitproject/src/gitproject/Main.java
@@ -0,0 +1,20 @@
+package gitproject;
+
+public class Main {
+	public static void main(String[] args)
+	{SalesData data= new SalesData();
+	displayGreeting();
+	data.Display();
+	
+	
+		
+	}
+
+	
+	private static void displayGreeting()
+	{
+		System.out.println("hello happy ");
+		System.out.println("this app show sales data ");
+		
+	}
+}
---
+++ gitproject/src/gitproject/SalesData.java
@@ -0,0 +1,9 @@
+package gitproject;
+
+public class SalesData {
+	int data[]={0,4,42};
+    public void Display()
+    {
+    	System.out.println("todo implement sales data");
+    }
+}
